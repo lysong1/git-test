62b2b3a008303346b37b2f5533190c547ea1b6f9
liuyusong <593457009@qq.com>
2022-03-23 19:18:18 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -4,7 +4,8 @@
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello github!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }
