023d94bc87c016938fcf2283a1282e05a511e955
liuyusong <593457009@qq.com>
2022-03-23 19:10:22 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,9 @@
+/**
+ * @author liuyusong
+ * @create 2022-03-23 19:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
